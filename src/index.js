62745c01081d553836366396295d677399b6fb66
Yashas <yashasjob@gmail.com>
2024-11-01 16:53:39 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+const rootElement = document.getElementById("root");
 
-
+// Render the App component on the root DOM node
+ReactDOM.render(<App />, rootElement);
